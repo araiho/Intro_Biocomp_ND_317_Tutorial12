68e038756a15649148affeacc3233600b1cbcf9d
Keith O'Connor <koconn23@nd.edu>
2017-11-22 18:32:32 -0500

    Exercise 12.1

--- Exercise12_1.R
+++ Exercise12_1.R
@@ -10,33 +10,36 @@ plot1 <- ggplot(chickwts, aes(x = feed, y = weight)) +
 
 plot1
 
+# Null Hypothesis : Soybeans and sunflower don't effect chicken weight.
+
+# Alternate hypothesis: That one will effect chicken weight more than the other.
+
 SoybeanandSunflower <- subset(chickwts, feed == "soybean"|feed == "sunflower")
 
-plot2 <- ggplot(data = SoybeanandSunflower, aes(x = feed, y = chickwts))+
-  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x) +
-  geom_point(size = 3, shape = 21, color = "black") +
-  theme_classic() +
-  xlab("feed") + ylab("weight") +
-  ggtitle("Chicken Feed vs. Weight") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  stat_smooth(method = "lm", se=FALSE, color="black") + 
-  geom_text(aes(x = 25, y = 400, label = lm_eqn1(lm(weight ~ feed, SoybeanandSunflower))), parse = TRUE)
-
-m= lm(weight ~ feed, SoybeanandSunflower)
-
-lm_eqn1 = function(m) {
-  
-  l <- list(a = format(coef(m)[1], digits = 2),
-            b = format(abs(coef(m)[2]), digits = 2),
-            r2 = format(summary(m)$r.squared, digits = 3));
-  
-  if (coef(m)[2] >= 0)  {
-    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
-  } else {
-    eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2,l)    
-  }
-  
-  as.character(as.expression(eq));                 
+nnlike0 <- function(p,x,y){
+  B0 = p[1]
+  sigma = exp(p[2])
+  expected = B0
+  nll= -sum(dnorm(x=y, mean=expected, sd = sigma, log = TRUE))
+  return(nll)
 }
 
-plot2
+nnlike1 <- function(p,x,y){
+  B0 = p[1]
+  B1 = p[2]
+  sigma = exp(p[3])
+  expected = B0+B1*x
+  nll1= -sum(dnorm(x=y, mean=expected, sd = sigma, log = TRUE))
+  return(nll1)
+}
+
+params0 <- c(400,100)
+params1 <- c(400,100,100)
+
+fit0 = optim(par = params0, fn=nnlike0, y=SoybeanandSunflower[(SoybeanandSunflower$feed%in%c("soybean","sunflower")),]$weight, x = 0)
+fit1 = optim(par = params1, fn=nnlike1, y=SoybeanandSunflower[(SoybeanandSunflower$feed%in%c("soybean","sunflower")),]$weight, x = 1)
+
+NullHypothesis <- pchisq(q = 2*abs((fit0$value-fit1$value)), df = 1, lower.tail = FALSE)
+
+NullHypothesis
+
